f9bee80f0bac080a4ce204fe03ba5b397be8f661
guihunkun <615815574@qq.com>
2020-09-28 22:30:12 +0800

    DrawNonzeroElementDistributionMap

---
+++ DrawNonzeroElementDistributionMap/drawNonzero.m
@@ -0,0 +1,5 @@
+clear
+clc
+
+load("A.mat")
+spy(A)
---
+++ DrawNonzeroElementDistributionMap/drawNonzero.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def read_file(file_name):
+	f = open(file_name)
+	lines = f.readlines()
+	K = np.zeros((len(lines), len(lines)))
+	i = 0
+	for line in lines:
+		line = line.strip().split('\t')
+		K[i, :] = np.array(list(map(int, line)))
+		i+=1
+	return K
+
+
+if __name__ == '__main__':
+	file_name = "A.txt"
+	K = read_file(file_name)
+	plt.matshow(K)
+	plt.show()
